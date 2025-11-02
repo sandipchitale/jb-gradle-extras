3177cdfee701be0e060ebd7f12dd9d51e54fedcc
Sandip Chitale <sandipchitale@gmail.com>
2025-11-02 12:09:28 -0800

    Initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "2.1.20"
+    id("org.jetbrains.intellij.platform") version "2.9.0"
+}
+
+group = "dev.sandipchitale"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+dependencies {
+    intellijPlatform {
+        intellijIdea("253.27864.23")
+        bundledPlugin("com.intellij.gradle")
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+
+
+        // Add plugin dependencies for compilation here, example:
+        // bundledPlugin("com.intellij.java")
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "253"
+        }
+
+        changeNotes = """
+            Initial version
+        """.trimIndent()
+    }
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+    }
+}
---
+++ src/main/java/dev/sandipchitale/jbgradleextras/GradleTaskHelpAction.java
@@ -0,0 +1,139 @@
+package dev.sandipchitale.jbgradleextras;
+
+import com.intellij.execution.executors.DefaultRunExecutor;
+import com.intellij.execution.process.ProcessOutputType;
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.externalSystem.model.ExternalSystemDataKeys;
+import com.intellij.openapi.externalSystem.model.ProjectSystemId;
+import com.intellij.openapi.externalSystem.model.execution.ExternalSystemTaskExecutionSettings;
+import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId;
+import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationListener;
+import com.intellij.openapi.externalSystem.service.execution.ProgressExecutionMode;
+import com.intellij.openapi.externalSystem.service.notification.ExternalSystemProgressNotificationManager;
+import com.intellij.openapi.externalSystem.util.ExternalSystemUtil;
+import com.intellij.openapi.externalSystem.view.ExternalSystemNode;
+import com.intellij.openapi.externalSystem.view.TaskNode;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.ui.components.JBScrollPane;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Collections;
+import java.util.List;
+
+public class GradleTaskHelpAction extends AnAction {
+
+    public static final ProjectSystemId GRADLE = new ProjectSystemId("GRADLE");
+
+    static class OutputDialog extends DialogWrapper {
+        private final String output;
+
+        public OutputDialog(Project project, String output, String taskName) {
+            super(project);
+            this.output = output;
+            setTitle("Task: " + taskName);
+
+            init();
+        }
+
+        @Nullable
+        @Override
+        protected JComponent createCenterPanel() {
+            JTextArea textArea = new JTextArea(output);
+            textArea.setEditable(false);
+            textArea.setLineWrap(true);
+            textArea.setWrapStyleWord(true);
+            JBScrollPane scrollPane = new JBScrollPane(textArea);
+            scrollPane.setPreferredSize(new Dimension(700, 550));
+            return scrollPane;
+        }
+
+        @Override
+        protected Action @NotNull [] createActions() {
+            // Only show OK button
+            Action okAction = getOKAction();
+            return new Action[]{okAction};
+        }
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+        Project project = anActionEvent.getProject();
+        List<ExternalSystemNode> selectedNodes= getSelectedTaskData(anActionEvent);
+        if (selectedNodes !=null && selectedNodes.size() > 0 && (selectedNodes.get(0) instanceof TaskNode taskNode)){
+            // Get the simple task name (e.g., "build")
+            String taskName = taskNode.getName();
+
+            ExternalSystemProgressNotificationManager notificationManager =
+                    ExternalSystemProgressNotificationManager.getInstance();
+
+            ExternalSystemTaskNotificationListener listener =
+                    new ExternalSystemTaskNotificationListener() {
+                        final StringBuilder output = new StringBuilder();
+
+                        @Override
+                        public void onTaskOutput(@NotNull ExternalSystemTaskId id, @NotNull String text, @NotNull ProcessOutputType outputType) {
+                            output.append(text);
+                        }
+
+                        @Override
+                        public void onEnd(@NotNull String projectPath, @NotNull ExternalSystemTaskId id) {
+                            notificationManager.removeNotificationListener(this);
+                            ApplicationManager.getApplication().invokeLater(() -> {
+                                new OutputDialog(project, output.toString(), taskName).show();
+                            });
+                        }
+                    };
+
+            notificationManager.addNotificationListener(listener);
+
+            // 1. Configure the task execution
+            ExternalSystemTaskExecutionSettings settings = new ExternalSystemTaskExecutionSettings();
+            settings.setExternalSystemIdString(GRADLE.getId());
+            settings.setExternalProjectPath(project.getBasePath());
+            settings.setTaskNames(Collections.singletonList("help")); // The task to run is 'help'
+            settings.setScriptParameters("--task " + taskName + " -q"); // The argument is '--task <name>'
+
+            // 2. Run the task
+            ExternalSystemUtil.runTask(
+                    settings,
+                    DefaultRunExecutor.EXECUTOR_ID, // Use the standard "Run" executor
+                    project,
+                    GRADLE,
+                    null,
+                    ProgressExecutionMode.IN_BACKGROUND_ASYNC,
+                    false,
+                    null);
+        }
+    }
+
+    @Override
+    public @NotNull ActionUpdateThread getActionUpdateThread() {
+        return ActionUpdateThread.EDT;
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent anActionEvent) {
+        List<ExternalSystemNode> selectedNodes = getSelectedTaskData(anActionEvent);
+        Presentation presentation = anActionEvent.getPresentation();
+        boolean taskNodeSelected = selectedNodes != null && selectedNodes.size() > 0 && (selectedNodes.get(0) instanceof TaskNode);
+        if (taskNodeSelected) {
+            presentation.setText("Detailed task information for: " + ((TaskNode) selectedNodes.get(0)).getName());
+        }
+        presentation.setEnabledAndVisible(taskNodeSelected);
+    }
+
+    /**
+     * Helper to get the TaskData if a single task is selected.
+     */
+    private List<ExternalSystemNode> getSelectedTaskData(@NotNull AnActionEvent anActionEvent) {
+        return anActionEvent.getData(ExternalSystemDataKeys.SELECTED_NODES);
+    }
+}
